4d70b509896ddc4e5d5035e540bd53c275c902b3
Yassir Brhaiberh <yassir.brh46@gmail.com>
2023-06-24 04:40:53 -0700

    Implements the rotr opcode.

--- monty.h
+++ monty.h
@@ -68,6 +68,7 @@ void mod(stack_t **stack, unsigned int line_number);
 void pchar(stack_t **stack, unsigned int line_number);
 void pstr(stack_t **stack, unsigned int line_number);
 void rotl(stack_t **stack, unsigned int line_number);
+void rotr(stack_t **stack, unsigned int line_number);
 void free_stack(stack_t *head);
 void addnode(stack_t **head, int n);
 void addqueue(stack_t **head, int n);
---
+++ rotr.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+/**
+ * rotr - Function
+ *
+ * Description: Rotates the stack to the bottom.
+ *
+ * @stack: Pointer to the stack.
+ * @line_number: Line number in the file.
+ *
+ * Return: No return because it's a void function.
+ */
+void rotr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *node;
+
+	(void)line_number;
+	while ((*stack)->next != NULL)
+	{
+		node = *stack;
+		*stack = (*stack)->next;
+	}
+}
